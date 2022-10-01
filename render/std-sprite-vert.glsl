6f6c85df62f7bc06345d92beda39edb84d0271dd
zergon321 <maximgradan@gmail.com>
2022-10-01 23:04:10 +0300

    Standard shaders edited.

--- render/std-sprite-vert.glsl
+++ render/std-sprite-vert.glsl
@@ -1,8 +1,8 @@
 #version 460 core
+
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
 
-uniform mat4 transform;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
